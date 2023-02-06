a8c9dccb7c729f6ae896978e03c8e561a3836b0b
Marc Esteve <marc.esteve.garcia@gmail.com>
2023-02-06 12:56:06 +0100

    javascript

---
+++ JS/primer.js
@@ -0,0 +1,6 @@
+alert("Hello world! en una ventana");
+alert(3);
+document.write("Hello world! escrito literal en el body");
+document.write(3);
+console.log("Hello world! en la consola");
+console.log(3);
