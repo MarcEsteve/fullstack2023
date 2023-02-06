1ec9e69df0df88e9a3064eab10c01cf03450c098
Marc Esteve <marc.esteve.garcia@gmail.com>
2023-02-06 14:46:41 +0100

    variables JS

--- JS/primer.js
+++ JS/primer.js
@@ -1,6 +1,10 @@
 alert("Hello world! en una ventana");
 alert(3);
-document.write("Hello world! escrito literal en el body");
+// document.write("Hello world! escrito literal en el body");
 document.write(3);
 console.log("Hello world! en la consola");
 console.log(3);
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+console.info('Info');
+console.warn('Warning dfsjkf dsjkfskjd f');
+console.error('Error jfdksjfbsdkjfsbdk');
---
+++ JS/variables.js
@@ -0,0 +1,15 @@
+var alert1 = 5;
+console.log(alert1);
+var alert1 = ''; // permitido
+
+let alert2 = 3;
+console.log(alert2);
+// no permitido porque ya se declaró antes
+alert2 = 3;
+alert2=15;// permitido
+
+//Constante
+const alert3 = 13;
+console.log(alert1);
+console.log(alert2);
+console.log(alert3);
