4c32a23508f0fb84a78628d3ed7e4d0aa57904b1
Marc Esteve <marc.esteve.garcia@gmail.com>
2023-02-07 09:12:00 +0100

    Update variables.js

--- JS/variables.js
+++ JS/variables.js
@@ -1,8 +1,11 @@
 var alert1 = 5;
-console.log(alert1);
-var alert1 = ''; // permitido
+console.log(alert1); //Número: 5
+var alert1 = 'jksdjad'; // permitido
 
-let alert2 = 3;
+var pepito; //DECLARAR = CREAR
+pepito = 17; // ASIGNAR = DEFINES VALOR Y TIPO
+
+let alert2 = 3; //DECLARACIÓN Y ASIGNACIÓN
 console.log(alert2);
 // no permitido porque ya se declaró antes
 alert2 = 3;
@@ -10,6 +13,6 @@ alert2=15;// permitido
 
 //Constante
 const alert3 = 13;
-console.log(alert1);
+console.log(alert1); //Texto: 'jksdjad'
 console.log(alert2);
 console.log(alert3);
