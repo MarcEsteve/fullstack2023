52958449e2fe2364afe88d40f084223fcf883189
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-02-13 11:37:33 +0100

    proyecto

---
+++ JS/ejproyecto.js
@@ -0,0 +1,38 @@
+//Ejercicio 1 de Proyecto
+let gestor = {
+    nombre:"Pepito",
+    dinero: 200,
+    numero: "Cuenta X"
+};
+let cliente = {
+    nombre: "Juanito",
+    numCuenta: 8074583224323,
+    saldoDisponible: 300
+};
+let mensaje = {
+    emisor: "Juanito",
+    receptor: "Pepito",
+    mensaje: "Hola gestor"
+};
+let transferencia = {
+    emisor: 4324234,
+    receptor: 5435435,
+    dineroTransferencia: 400
+};
+//Ejercicio 2 de Proyecto
+let gestor3 = Object.keys(gestor);
+for (const key of gestor3) {
+    console.log(key);
+}
+let cliente3 = Object.keys(cliente);
+for (const key of cliente3) {
+    console.log(key);
+}
+let mensaje3 = Object.keys(mensaje);
+for (const key of mensaje3) {
+    console.log(key);
+}
+let transferencia3 = Object.keys(transferencia);
+for (const key of transferencia3) {
+    console.log(key);
+}
